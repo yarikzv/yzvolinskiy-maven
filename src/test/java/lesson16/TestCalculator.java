e205aebd369da26b1f86eb409cb82dcca27b89da
John Doe <johndoe@example.com>
2021-10-07 21:17:13 +0300

    gitignore

--- src/test/java/lesson16/TestCalculator.java
+++ src/test/java/lesson16/TestCalculator.java
@@ -8,7 +8,10 @@ public class TestCalculator {
     @Test
     public void test_sum_normal_case(){
         Assertions.assertEquals(4,Calculator.sum(3,1));
-
+    }
+    @Test
+    public void test_sum_divide_case(){
+        Assertions.assertThrows(ArithmeticException.class, () -> Calculator.divide(3,0));
     }
 
 }
