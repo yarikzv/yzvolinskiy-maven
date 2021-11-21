6f653e052df6d96bee39ce45783e6676cd8b06e5
yarikzv <yzv.yaroslav@gmail.com>
2021-11-21 20:14:46 +0200

    Homework to lesson 27. Changed variable for connection login and pass.

--- sql/src/main/java/homework27/ConnectionUtil.java
+++ sql/src/main/java/homework27/ConnectionUtil.java
@@ -4,10 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * An util singleton for creating connection to database server.
+ * Uses system environment variables {@code SQL_LOGIN} and {@code SQL_PASS}
+ * for access to MySQL server.
+ */
 public class ConnectionUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/university";
-    private static final String LOGIN = "root";
-    private static final String PASS = "yarik150883";
+    private static final String LOGIN = System.getenv("SQL_LOGIN");
+    private static final String PASS = System.getenv("SQL_PASS");
     private static final Connection connection = buildConnection();
 
     private static Connection buildConnection() {
@@ -19,10 +24,12 @@ public class ConnectionUtil {
         }
     }
 
+    // Returns connection
     public static Connection getConnection(){
         return connection;
     }
 
+    // Connection close
     public static void shutdown(){
         try {
             if (connection != null) {
